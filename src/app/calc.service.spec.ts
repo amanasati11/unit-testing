e1ef1085b8593cf9ede5f696cd068679b2a6ed1c
Aman Asati <amanasati2@gmail.com>
2024-04-06 17:12:42 +0530

    feat: spy | spyOn | createSpyObject

--- src/app/calc.service.spec.ts
+++ src/app/calc.service.spec.ts
@@ -1,9 +1,21 @@
 import { CalcService } from './calc.service';
+import { SharedService } from './shared.service';
 
 describe('CalcService', () => {
   it('should multiply two numbers', () => {
-    const calc = new CalcService();
+    const shared = new SharedService();
+    const calc = new CalcService(shared);
     const result = calc.multiply(2, 10);
     expect(result).toBe(20);
+  });
+
+  it('should call the mySharedFuntion', () => {
+    // const shared = new SharedService();
+    const shared = jasmine.createSpyObj("SharedService", ["mySharedFunction"]);
+    const calc = new CalcService(shared);
+    // spyOn(shared, 'mySharedFunction');
+    // spyOn(shared, 'mySharedFunction').and.callThrough();
+    const result = calc.multiply(2, 10);
+    expect(shared.mySharedFunction).toHaveBeenCalled();
   });
 });
